f5131967a86ef28910a35186fe668b27c08ac08b
meanllbrl <meanllbrl@gmail.com>
2024-09-25 11:53:58 +0000

    update

--- lib/src/constants.dart
+++ lib/src/constants.dart
@@ -1,4 +1,7 @@
+// A class to hold constant values.
 class Constants {
+  // The maximum number of geohashes to use when surrounding a point.
   static int surroundGeohashLimit = 10;
+  // The maximum number of geohashes to use when bounding a map.
   static int mapBoundGeohashLimit = 20;
 }
